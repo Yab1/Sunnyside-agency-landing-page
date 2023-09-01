322b7bd4ad26cdb91800aa25872f623cd49af659
Yab1 <yabilisanu@gmail.com>
2023-09-02 01:49:13 +0300

    Tailwind: custom color updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,19 @@ module.exports = {
     extend: {
       colors: {
         "Soft-red": "hsl(7, 99%, 70%)",
-        "Dark-desaturated-cyan ": "hsl(167, 40%, 24%)", //graphic design text
+        "Dark-desaturated-cyan": "hsl(167, 40%, 24%)", // footer
         "Dark-blue": "hsl(198, 62%, 26%)", //photography text
-        "Dark-moderate-cyan": "hsl(168, 34%, 41%)", // footer
-        "Very dark-desaturated blue": "hsl(212, 27%, 19%)",
-        "Very-dark-grayish blue": "hsl(213, 9%, 39%)",
+        "Dark-moderate-cyan": "hsl(168, 34%, 41%)", // graphic design text
+        "Very-dark-desaturated-blue": "hsl(212, 27%, 19%)",
+        "Very-dark-grayish-blue": "hsl(213, 9%, 39%)",
         "Dark-grayish-blue": "hsl(232, 10%, 55%)",
         "Grayish-blue": "hsl(210, 4%, 67%)",
         Yellow: "hsl(51, 100%, 49%)",
         White: "hsl(0, 0%, 100%)",
       },
       fontFamily: {
-        Barlow: ["Barlow", "sans-serif"],
-        Fraunces: ["Fraunces", "serif"],
+        Barlow: ["Barlow"],
+        Fraunces: ["Fraunces"],
       },
     },
   },
